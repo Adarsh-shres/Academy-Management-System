4790c9db35b15a8c8e4af2c863a21b71e7f4207d
Beezay98 <sk448766@gmail.com>
2026-04-06 11:32:53 +0545

    feat(student-profile): build student profile and settings page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,6 +16,7 @@ import StudentCoursesPage from './pages/StudentCoursesPage.tsx';
 import StudentSchedulePage from './pages/StudentSchedulePage.tsx';
 import StudentAssignmentsPage from './pages/StudentAssignmentsPage.tsx';
 import StudentAttendancePage from './pages/StudentAttendancePage.tsx';
+import StudentProfilePage from './pages/StudentProfilePage.tsx';
 import RegisterStudentsPage from './pages/RegisterStudentsPage.tsx';
 
 function UnderDevelopment() {
@@ -56,6 +57,7 @@ function App() {
           <Route path="/student/schedule" element={<StudentSchedulePage />} />
           <Route path="/student/assignments" element={<StudentAssignmentsPage />} />
           <Route path="/student/attendance" element={<StudentAttendancePage />} />
+          <Route path="/student/profile" element={<StudentProfilePage />} />
         </Route>
       </Routes>
     </Router>
